8daf28274cfa006cacc78faeaa09030cf18105b3
Jie Zhu <chore.08-protist@icloud.com>
2024-11-24 21:02:17 -0600

    support single direct fuzz

--- magma/apply_patches.sh
+++ magma/apply_patches.sh
@@ -7,10 +7,25 @@ set -e
 ##
 
 # TODO filter patches by target config.yaml
-find "$TARGET/patches/setup" "$TARGET/patches/bugs" -name "*.patch" | \
+find "$TARGET/patches/setup" -name "*.patch" | \
 while read patch; do
     echo "Applying $patch"
     name=${patch##*/}
     name=${name%.patch}
     sed "s/%MAGMA_BUG%/$name/g" "$patch" | patch -p1 -d "$TARGET/repo"
 done
+
+if [ -z "$1" ]; then
+    echo "No patch name provided."
+    exit 1
+fi
+
+patch_file="$TARGET/patches/bugs/$1.patch"
+if [ ! -f "$patch_file" ]; then
+    echo "Patch file $patch_file not found."
+    exit 1
+fi
+
+echo "Applying bug patch: $patch_file"
+name=${1%.patch}
+sed "s/%MAGMA_BUG%/$name/g" "$patch_file" | patch -p1 -d "$TARGET/repo"
