e764d3b9c53e2da9e36890b1fa671ccee39e41bf
jiezhuzzz <jiezzz@duck.com>
2024-12-12 16:11:26 -0600

    update

--- setup.sh
+++ setup.sh
@@ -5,5 +5,3 @@ sudo apt update && sudo apt install -y util-linux inotify-tools docker.io git
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
-
-git clone https://github.com/jiezhuzzz/Titan.git fuzzers/titan
