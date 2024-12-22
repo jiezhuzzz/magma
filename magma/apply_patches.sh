647fa98a87f0258b0e07a9f2659753bad86b16ba
jiezhuzzz <jiezzz@duck.com>
2024-12-22 16:25:50 -0600

    hotfix: apply patch

--- magma/apply_patches.sh
+++ magma/apply_patches.sh
@@ -27,5 +27,7 @@ patch -p1 -d "$TARGET/repo" < "$base_patch_file"
 spec_patch_file="$TARGET/patches/specs/${1}.patch"
 if [ ! -f "$spec_patch_file" ]; then
     echo -e "\e[31mPatch file $spec_patch_file not found. Only applying base patch.\e[0m"
+else
+    patch -p1 -d "$TARGET/repo" < "$spec_patch_file"
+    echo -e "\e[32mPatch file $spec_patch_file applied.\e[0m"
 fi
-patch -p1 -d "$TARGET/repo" < "$spec_patch_file"
