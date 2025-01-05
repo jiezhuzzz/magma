2c1e09d10d3e5b035b4b26868ef926a9fbf98ec0
jiezhuzzz <jiezzz@duck.com>
2025-01-05 03:08:58 -0600

    patches: llm baseline libpng, libsndfile

--- magma/apply_patches.sh
+++ magma/apply_patches.sh
@@ -17,12 +17,7 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-# copy generated function level fuzzing targets list
-fuzzing_targets_file="$TARGET/patches/fuzzing_targets/$1.txt"
-if [ -f "$fuzzing_targets_file" ]; then
-    cp "$fuzzing_targets_file" "$TARGET/repo/fuzzing_targets.txt"
-fi
-
+# apply base patch
 base_patch_file="$TARGET/patches/bugs/${1%%-*}.patch"
 if [ ! -f "$base_patch_file" ]; then
     echo "Patch file $base_patch_file not found."
@@ -30,3 +25,12 @@ if [ ! -f "$base_patch_file" ]; then
 fi
 patch -p1 -d "$TARGET/repo" < "$base_patch_file"
 echo "Base patch file $base_patch_file applied."
+
+# apply fuzzing patch
+fuzzing_patch_file="$TARGET/patches/specs/${1}.patch"
+if [ ! -f "$fuzzing_patch_file" ]; then
+    echo "Patch file $fuzzing_patch_file not found."
+    exit 1
+fi
+patch -p1 -d "$TARGET/repo" < "$fuzzing_patch_file"
+echo "Fuzzing patch file $fuzzing_patch_file applied."
